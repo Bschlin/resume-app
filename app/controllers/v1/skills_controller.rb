2b2c5a2dbea92d8802f668a1e4c025ac681f0211
Zack Frank <frank.zack@gmail.com>
2018-05-01 19:16:30 -0500

    Add Experiences and Skills controllers

---
+++ app/controllers/v1/skills_controller.rb
@@ -0,0 +1,34 @@
+class V1::SkillsController < ApplicationController
+  def index
+    skill = Skill.find_by(id: params[:id])
+    render json: skill.as_json
+  end
+
+  def create
+    Skill.create(
+      skill: params[:skill]
+      )
+    render json: skill.as_json
+  end
+
+  def update
+    skill = Skill.find_by(id: params[:id])
+
+    skill.skill = params[:skill] || skill.skill
+
+    skill.save
+    render json: skill.as_json
+  end
+
+  def show
+    skill = Skill.find_by(id: params[:id])
+    render json: skill.as_json
+  end
+
+  def destroy
+    skill = Skill.find_by(id: params[:id])
+    skill.destroy
+    render json: {message: "Skill successfully destroyed."}
+  end
+
+end
