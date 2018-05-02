9f4af37239b98a428879dbccb6f59a63687832ca
Vincent Kwok <vkwok22@gmail.com>
2018-05-01 19:00:20 -0500

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,28 @@ Rails.application.routes.draw do
   namespace :v1 do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     get "/students" => "students#index"
+    post "/students" => "students#create"
+    get "/students/id" => "students#show"
+    patch "/students/id" => "students#update"
+    delete "/students/id" => "students#destroy"
 
     get "/experiences" => "experiences#index"
+    post "/experiences" => "experiences#create"
+    get "/experiences/id" => "experiences#show"
+    patch "/experiences/id" => "experiences#update"
+    delete "/experiences/id" => "experiences#destroy"
+
+    get "/educations" => "educations#index"
+    post "/educations" => "educations#create"
+    get "/educations/id" => "educations#show"
+    patch "/educations/id" => "educations#update"
+    delete "/educations/id" => "educations#destroy"
+
+    get "/skills" => "skills#index"
+    post "/skills" => "skills#create"
+    get "/skills/id" => "skills#show"
+    patch "/skills/id" => "skills#update"
+    delete "/skills/id" => "skills#destroy"
+
   end
 end
